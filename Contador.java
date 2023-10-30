063f7ef7f4600c9d183f9971f793b8f17c4059b0
DiegoMuniz92 <diegooliveiramuniz2014@gmail.com>
2023-10-30 19:20:26 -0400

    Finalizando projeto

---
+++ Contador.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        
+        Scanner terminal = new Scanner(System.in);
+        int parametroUm = terminal.nextInt();
+        int parametroDois = terminal.nextInt();
+        System.out.println(" ");
+
+        try {
+            conta(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosExceptions exceptions) {
+            System.out.println("Informe o segundo valor maior que o primeiro.");
+        }
+    }    
+
+    static void conta (int parametroUm, int parametroDois) throws ParametrosInvalidosExceptions {
+        int contar = parametroDois - parametroUm;
+        int contador = 1;
+
+        if (parametroDois > parametroUm) {
+           while (contador <= contar) {
+            System.out.println("Imprimindo o número: " + contador);
+            contador++;
+            } 
+        } else {
+            throw new ParametrosInvalidosExceptions();
+        }
+        
+        
+    }
+}
